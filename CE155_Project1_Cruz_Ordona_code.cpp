e26e4234b8ca7a5ea50720eb19f4bf691a73eeae
Kyle Ordona <janzrel.ordona@obf.ateneo.edu>
2020-03-17 22:40:13 +0800

    done with fcfs

--- CE155_Project1_Cruz_Ordona_code.cpp
+++ CE155_Project1_Cruz_Ordona_code.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -15,15 +16,15 @@ enum algo_list {
 
 algo_list hashit(string const& algo);
 
-void solveFCFS(int n, int arrival[], int burst[], int prio[]);
-void solveSJF(int n, int arrival[], int burst[], int prio[]);
-void solveSRTF(int n, int arrival[], int burst[], int prio[]);
-void solveP(int n, int arrival[], int burst[], int prio[]);
-void solveRR(int n, int arrival[], int burst[], int prio[], int q);
+void solveFCFS(int n, vector<int> arrival, vector<int> burst);
+void solveSJF(int n, vector<int> arrival, vector<int> burst, vector<int> prio);
+void solveSRTF(int n, vector<int> arrival, vector<int> burst, vector<int> prio);
+void solveP(int n, vector<int> arrival, vector<int> burst, vector<int> prio);
+void solveRR(int n, vector<int> arrival, vector<int> burst, vector<int> prio, int q);
 
 int main() {
   string line;
-  string input;
+  int temp;
   ifstream myfile("CE155_Project1_Cruz_Ordona_testfile.txt");
 
   if(myfile.is_open()) {
@@ -31,34 +32,37 @@ int main() {
     myfile >> num_test_cases;
 
     for(int test_case = 1; test_case <= num_test_cases; test_case++) {
-      cout << test_case << '\n';
+      // cout << test_case << endl;
       
       int num_processes;
       string algo;
       int q;
-      int arrival_times[num_processes];
-      int burst_times[num_processes];
-      int priority_list[num_processes];
+      vector<int> arrival_times;
+      vector<int> burst_times;
+      vector<int> priority_list;
 
       myfile >> num_processes;
       myfile >> algo;
-      // cout << num_processes << " " << algo;
+      // cout << num_processes << " " << algo << endl;
 
       if(algo == "RR") {
         myfile >> q;
-        // cout << " " << q;
+        // cout << " " << q << endl;
       }
 
       for(int p = 0; p < num_processes; p++) {
-        myfile >> arrival_times[p];
-        myfile >> burst_times[p];
-        myfile >> priority_list[p];
-        // cout << '\n' << arrival_times[p] << " " << burst_times[p] << " " << priority_list[p];
+        myfile >> temp;
+        arrival_times.push_back(temp);
+        myfile >> temp;
+        burst_times.push_back(temp);
+        myfile >> temp;
+        priority_list.push_back(temp);
+        // cout << arrival_times[p] << " " << burst_times[p] << " " << priority_list[p] << endl;
       }
 
       switch(hashit(algo)) {
         case FCFS:
-          solveFCFS(num_processes, arrival_times, burst_times, priority_list);
+          solveFCFS(num_processes, arrival_times, burst_times);
           break;
         case SJF:
           solveSJF(num_processes, arrival_times, burst_times, priority_list);
@@ -84,45 +88,47 @@ int main() {
 }
 
 
-void solveFCFS(int n, int arrival[], int burst[], int prio[]) {
-  int sorted_arrival[n];
-  for(int i = 0; i < n; i++) {
-    sorted_arrival[i] = arrival[i];
-  };
-
-  sort(sorted_arrival, sorted_arrival+n);
-
+void solveFCFS(int n, vector<int> arrival, vector<int> burst) {
+  vector<int> sorted_arrival(arrival);
+  sort(sorted_arrival.begin(), sorted_arrival.end());
+  int time_elapsed = sorted_arrival.at(0);
   for (int i = 0; i < n; i++) {
-    int index = distance(arrival, find(arrival, arrival + n, sorted_arrival[i]));
-    cout << arrival[index] << " " << index + 1 << " " << burst[index] << "X" << '\n';
-  };
-};
+    vector<int>::iterator it = find(arrival.begin(), arrival.end(), sorted_arrival.at(i));
+    int index = distance(arrival.begin(), it);
+    time_elapsed = max(arrival.at(index), time_elapsed);
 
+    cout << time_elapsed << " " << index + 1 << " " << burst.at(index) << "X" << endl;
 
-void solveSJF(int n, int arrival[], int burst[], int prio[]) {
-  for (int i = 0; i < n; i++) {
-    cout << arrival[i] << " " << burst[i] << " " << prio[i] << '\n';
+    time_elapsed += burst.at(index);
+    arrival.at(index) = -1;
   };
 };
 
 
-void solveSRTF(int n, int arrival[], int burst[], int prio[]) {
+void solveSJF(int n, vector<int> arrival, vector<int> burst, vector<int> prio) {
   for (int i = 0; i < n; i++) {
-    cout << arrival[i] << " " << burst[i] << " " << prio[i] << '\n';
+    cout << arrival.at(i) << " " << burst.at(i) << " " << prio.at(i) << '\n';
   };
 };
 
 
-void solveP(int n, int arrival[], int burst[], int prio[]) {
+void solveSRTF(int n, vector<int> arrival, vector<int> burst, vector<int> prio) {
   for (int i = 0; i < n; i++) {
-    cout << arrival[i] << " " << burst[i] << " " << prio[i] << '\n';
+    cout << arrival.at(i) << " " << burst.at(i) << " " << prio.at(i) << '\n';
   };
 };
 
 
-void solveRR(int n, int arrival[], int burst[], int prio[], int q) {
+void solveP(int n, vector<int> arrival, vector<int> burst, vector<int> prio) {
   for (int i = 0; i < n; i++) {
-    cout << arrival[i] << " " << burst[i] << " " << prio[i] << '\n';
+    cout << arrival.at(i) << " " << burst.at(i) << " " << prio.at(i) << '\n';
+  };
+};
+
+
+void solveRR(int n, vector<int> arrival, vector<int> burst, vector<int> prio, int q) {
+  for (int i = 0; i < n; i++) {
+    cout << arrival.at(i) << " " << burst.at(i) << " " << prio.at(i) << '\n';
   };
 };
 
